477017b1021498300edab0456298cb4255cedfd3
Michael Hsu <michael100.hsu@gmail.com>
2016-12-24 22:31:37 -0600

    added tips_and_tricks_.txt file, added paddle and movement, physics

---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	int left, right;
+	KeyCode leftKey, rightKey;
+
+	void Awake() {
+
+		// Initialize KeyCodes
+		left = PlayerPrefs.GetInt ("block-breaker-left-control");
+		right = PlayerPrefs.GetInt ("block-breaker-right-control");
+		leftKey = (KeyCode)left;
+		rightKey = (KeyCode)right;
+	}
+	// Update is called once per frame
+	void Update () {
+
+		// Get current position, change it, then set current position to altered position
+		if(Input.GetKey(leftKey)) {
+			Vector2 position = this.transform.position;
+			position.x = position.x - (float).1;
+			this.transform.position = position;
+		}
+		if(Input.GetKey(rightKey)) {
+			Vector2 position = this.transform.position;
+			position.x = position.x + (float).1;
+			this.transform.position = position;
+		}
+	}
+}
