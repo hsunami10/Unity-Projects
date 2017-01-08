538c97cb0c7657216c0b2d2f1c83d633ee578712
Michael Hsu <michael100.hsu@gmail.com>
2017-01-08 11:55:36 -0600

    added mobile tsfh app

---
+++ Two Steps From Hell/Assets/Background.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Background : MonoBehaviour {
+
+	private float duration, colorAlpha;
+	private Color normColor;
+
+	// Use this for initialization
+	void Start () {
+		duration = 1.5f;
+		colorAlpha = 1f;
+		normColor = gameObject.GetComponent<SpriteRenderer> ().color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (PlayerPrefs.GetString ("tsfh fade out") == "true") {
+			duration -= Time.deltaTime;
+
+			// Dim background once title and caption fade out
+			if (duration >= 0) {
+				colorAlpha -= 0.009f;
+				normColor.a = colorAlpha;
+				gameObject.GetComponent<SpriteRenderer> ().color = normColor;
+			}
+		}
+	}
+}
---
+++ Two Steps From Hell/Assets/FadeInOutTitle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInOutTitle : MonoBehaviour {
+
+	public Text text;
+	private float colorAlpha, animationLength, animationFadeOut;
+
+	// Use this for initialization
+	void Start () {
+		PlayerPrefs.SetString ("tsfh fade out", "false");
+		text.text = "Two Steps From Hell";
+		colorAlpha = 0f;
+		animationLength = 1.5f;
+		animationFadeOut = 1.5f;
+		text.color = new Color (255, 255, 255, colorAlpha);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animationLength -= Time.deltaTime;
+
+		// Fade out animation
+		if(PlayerPrefs.GetString("tsfh fade out") == "true") {
+			animationFadeOut -= Time.deltaTime;
+
+			if(animationFadeOut >= 0) {
+				colorAlpha -= 0.03f;
+				text.color = new Color (255, 255, 255, colorAlpha);
+			}
+		}
+
+		// Fade in duration: 1.5 sec
+		else if(animationLength >= 0) {
+			colorAlpha += 0.02f;
+			text.color = new Color (255, 255, 255, colorAlpha);
+		}
+
+		// Destroy when faded out
+		if (colorAlpha <= 0.05f && PlayerPrefs.GetString("tsfh fade out") == "true")
+			Destroy (gameObject);
+	}
+}
